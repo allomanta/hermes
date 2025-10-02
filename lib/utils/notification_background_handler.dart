6350c8fcd4540ff65346d18d36eb7e3a9ea1ab31
Steven Lageveen <stevenmax888@gmail.com>
2025-10-02 21:53:59 +0200

    Reduce diff with upstream somewhat
    Rebase fixes
    
    Rebase fixes

--- lib/utils/notification_background_handler.dart
+++ lib/utils/notification_background_handler.dart
@@ -8,13 +8,13 @@ import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:fluffychat/l10n/l10n.dart';
-import 'package:fluffychat/utils/client_manager.dart';
-import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
-import 'package:fluffychat/utils/platform_infos.dart';
-import 'package:fluffychat/utils/push_helper.dart';
+import 'package:hermes/l10n/l10n.dart';
+import 'package:hermes/utils/matrix_sdk_extensions/matrix_locals.dart';
+import 'package:hermes/utils/platform_infos.dart';
+import 'package:hermes/utils/push_helper.dart';
 import '../config/app_config.dart';
 import '../config/setting_keys.dart';
+import 'package:hermes/utils/client_manager.dart';
 
 bool _vodInitialized = false;
 
@@ -96,7 +96,7 @@ Future<void> notificationTap(
     notificationResponse.notificationResponseType.name,
   );
   final payload =
-      FluffyChatPushPayload.fromString(notificationResponse.payload ?? '');
+      HermesPushPayload.fromString(notificationResponse.payload ?? '');
   switch (notificationResponse.notificationResponseType) {
     case NotificationResponseType.selectedNotification:
       final roomId = payload.roomId;
@@ -120,7 +120,7 @@ Future<void> notificationTap(
             : '/rooms/$roomId',
       );
     case NotificationResponseType.selectedNotificationAction:
-      final actionType = FluffyChatNotificationActions.values.singleWhereOrNull(
+      final actionType = HermesNotificationActions.values.singleWhereOrNull(
         (action) => action.name == notificationResponse.actionId,
       );
       if (actionType == null) {
@@ -140,13 +140,13 @@ Future<void> notificationTap(
         );
       }
       switch (actionType) {
-        case FluffyChatNotificationActions.markAsRead:
+        case HermesNotificationActions.markAsRead:
           await room.setReadMarker(
             payload.eventId ?? room.lastEvent!.eventId,
             mRead: payload.eventId ?? room.lastEvent!.eventId,
             public: AppConfig.sendPublicReadReceipts,
           );
-        case FluffyChatNotificationActions.reply:
+        case HermesNotificationActions.reply:
           final input = notificationResponse.input;
           if (input == null || input.isEmpty) {
             throw Exception(
@@ -186,7 +186,7 @@ Future<void> notificationTap(
                   enableVibration: false,
                   actions: <AndroidNotificationAction>[
                     AndroidNotificationAction(
-                      FluffyChatNotificationActions.reply.name,
+                      HermesNotificationActions.reply.name,
                       l10n.reply,
                       inputs: [
                         AndroidNotificationActionInput(
@@ -198,14 +198,14 @@ Future<void> notificationTap(
                       semanticAction: SemanticAction.reply,
                     ),
                     AndroidNotificationAction(
-                      FluffyChatNotificationActions.markAsRead.name,
+                      HermesNotificationActions.markAsRead.name,
                       l10n.markAsRead,
                       semanticAction: SemanticAction.markAsRead,
                     ),
                   ],
                 ),
               ),
-              payload: FluffyChatPushPayload(
+              payload: HermesPushPayload(
                 client.clientName,
                 room.id,
                 eventId,
@@ -216,4 +216,4 @@ Future<void> notificationTap(
   }
 }
 
-enum FluffyChatNotificationActions { markAsRead, reply }
+enum HermesNotificationActions { markAsRead, reply }
