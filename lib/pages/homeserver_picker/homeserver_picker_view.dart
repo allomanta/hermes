24b6969584c3cf3ebee63048c63f9e10a92fad32
Steven Lageveen <stevenmax888@gmail.com>
2025-10-04 19:00:33 +0200

    Fix login view on desktop

--- lib/pages/homeserver_picker/homeserver_picker_view.dart
+++ lib/pages/homeserver_picker/homeserver_picker_view.dart
@@ -118,7 +118,7 @@ class HomeserverPickerView extends StatelessWidget {
                         ),
                       ),
                     ),
-                    const SizedBox(height: 32),
+                    const Spacer(),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 32.0),
                       child: SelectableLinkify(
@@ -133,7 +133,6 @@ class HomeserverPickerView extends StatelessWidget {
                         onOpen: (link) => launchUrlString(link.url),
                       ),
                     ),
-                    const Spacer(),
                     Padding(
                       padding: const EdgeInsets.all(32.0),
                       child: Column(
