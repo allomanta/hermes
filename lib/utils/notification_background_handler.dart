6d7326721e76132b6be9376594afca7ad6872e8c
Steven Lageveen <stevenmax888@gmail.com>
2025-10-11 17:26:31 +0200

    rebase fixes

--- lib/utils/notification_background_handler.dart
+++ lib/utils/notification_background_handler.dart
@@ -7,7 +7,7 @@ import 'package:flutter_vodozemac/flutter_vodozemac.dart' as vod;
 import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:fluffychat/utils/client_download_content_extension.dart';
+import 'package:hermes/utils/client_download_content_extension.dart';
 import 'package:hermes/l10n/l10n.dart';
 import 'package:hermes/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:hermes/utils/platform_infos.dart';
