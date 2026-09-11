bf2c95d51304804220858322041e9ac85ddd9c89
allomanta <steven@lageveen.co>
2026-09-11 17:48:48 +0200

    Retry setting notification read

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -731,7 +731,7 @@ class ChatController extends State<ChatPageWithRoom>
           eventId: eventId,
           public: AppSettings.sendPublicReadReceipts.value,
         )
-        .then((_) {
+        .whenComplete(() {
           _setReadMarkerFuture = null;
         });
   }
